517f8abd8ed70a32be40a9666106982a919b048a
Luke Cleland <lukecleland@gmail.com>
2023-02-11 02:44:17 +1100

    .

--- speechtoimage/speech.js
+++ speechtoimage/speech.js
@@ -1,6 +1,6 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: `${process.env.OPENAI_KEY}`,
+  apiKey: `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`,
 });
 const openai = new OpenAIApi(configuration);
 
