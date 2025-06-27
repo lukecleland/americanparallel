be6bf9f77959f9c4f35e05ec098a92c94b5fe517
Luke Cleland <lukecleland@gmail.com>
2025-06-27 12:42:30 +1000

    .

--- r3f/delorean/vite.config.js
+++ r3f/delorean/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "",
   esbuild: {
     pure: ["console.log"], // example: have esbuild remove any console.log
     minifyIdentifiers: false, // but keep variable names
